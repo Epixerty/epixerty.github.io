1bbbe0471626e77fba80edbb9536d8e6841f6331
epix <71335972+Epixerty@users.noreply.github.com>
2023-12-25 11:29:15 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 // The URL you provided
+function updateTime() {
+    const currentTime = new Date().toLocaleTimeString('en-US', { timeZone: 'CET', hour12: false });
+    document.getElementById('currentTime').textContent = currentTime;
+  }
+
+  // Update the time initially
+  updateTime();
+
+  // Update the time every second (1000 milliseconds)
+  setInterval(updateTime, 1000);
